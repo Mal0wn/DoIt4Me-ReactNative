4f9fc8c1561b68a2a4db998b0e16d3341edac8eb
Marine M <marine.michelot-cic@ibm.com>
2022-12-13 10:07:11 +0100

    add component mission

---
+++ src/components/organismes/Missions/Mission.tsx
@@ -0,0 +1,111 @@
+import  React , { useState } from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Pressable,
+  Image
+} from 'react-native';
+
+
+export interface Props {
+  title: string;
+  desc : string;
+  price : number;
+
+}
+
+export class MissionComp extends React.Component<Props> {
+
+	constructor(props: Props) {
+    super(props);
+
+    
+  }
+
+  render() {
+    return (
+	<Pressable style={styles.mission}>
+		<View style={styles.container}>
+			<Text style={styles.title}>
+			{this.props.title }
+			</Text>
+			<View style={styles.desc}>
+				<View style={styles.containImg}>
+					<Image
+				style={styles.image}
+				source={{uri: 'https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=iQEwihUn'}}
+				/>
+				</View>
+				<Text> {this.props.desc}</Text>
+				<Text style={styles.price}> {this.props.price} €/h </Text>
+			</View>
+		</View>
+	</Pressable>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mission: {
+	backgroundColor : "#ffffff",
+	borderTopLeftRadius: 20,
+	borderTopRightRadius : 20
+  },
+  container: {
+  backgroundColor : "#ffffff",
+    flex: 1,
+    justifyContent: "center",
+	borderTopLeftRadius: 20,
+	borderTopRightRadius : 20
+	
+  },
+  containImg: {
+	width : 300,
+	height : 150,
+	marginTop : 20,
+	flex : 1,
+	justifyContent : "center"
+  },
+  title: {
+    fontSize: 22,
+	textAlign: "center",
+	backgroundColor : "#5C9EAD",
+	color : "#222823",
+	padding : 10,
+	borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  desc: {
+   backgroundColor : "#CDB4DB",
+   color : "#222823",
+   minHeight : 300,
+   padding : 20,
+	borderBottomLeftRadius: 20,
+	borderBottomRightRadius : 20
+  },
+  image: {
+	width : "100%",
+	height : "100%",
+	objectFit : "cover",
+	display : "flex",
+	justifyContent : "center",
+	marginBottom: 20,
+	borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+	borderBottomLeftRadius: 20,
+	borderBottomRightRadius : 20
+  }, 
+  price : {
+	fontWeight: '900',
+	paddingTop : 10
+  }
+ 
+
+});
---
+++ src/scenes/Home/HomeScreen.tsx
@@ -0,0 +1,62 @@
+import  React  from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  FlatList
+} from 'react-native';
+
+import { MissionComp } from '../../components/organismes/Missions/Mission';
+
+const DATA = [
+{
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+    desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+    desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+    desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+  },
+]
+
+export const HomeScreen = () => {
+
+  return (
+    <ScrollView style={styles.scrollView}>
+       <MissionComp title={'Garder mon chien'} desc={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'} price={40}
+        
+       ></MissionComp>
+       
+       
+      </ScrollView>
+
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+});
+
